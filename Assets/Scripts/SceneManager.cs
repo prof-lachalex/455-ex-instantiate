f0f5a512fc2cd331d06372f7a1bad3a333a5873f
prof-lachalex <alexandre.lachapelle@claurendeau.qc.ca>
2024-02-29 15:43:55 -0500

    Prefab Cylindrique; Choix aléatoire de prefab; Ajout d'un 2e compteur

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 public class SceneManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _prefabToSpawn;
+    private GameObject[] _prefabsToSpawn;
     List<Transform> _spawnPoints;
 
-    private int _nbObjects;
-    TextMeshProUGUI _objectCounterText;
+    private List<int> _nbObjects;
+    private List<TextMeshProUGUI> _objectCounterTexts;
 
     private float _timer;
     [SerializeField]
@@ -19,7 +19,6 @@ public class SceneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _nbObjects = 0;
         _timer = 0;
         _spawnPoints = new List<Transform>( GameObject.Find("SpawnPoints").GetComponentsInChildren<Transform>() );
         if (_spawnPoints.Count == 0)
@@ -27,12 +26,25 @@ public class SceneManager : MonoBehaviour
             Debug.LogError("SceneManager: Aucun Spawn point");
         }
 
-        _objectCounterText = GameObject.Find("ObjectCounterText").GetComponent<TextMeshProUGUI>();
-        if(_objectCounterText == null)
+        _objectCounterTexts = new List<TextMeshProUGUI>(GameObject.Find("ObjectCounterCanvas").GetComponentsInChildren<TextMeshProUGUI>());
+        if(_objectCounterTexts == null || _objectCounterTexts.Count == 0)
         {
-            Debug.LogError("SceneManager: TMPro pour le compteur mal ou pas spécifié");
+            Debug.LogError("SceneManager: Conteneur des objets TMPro pour les compteurs non spécifié");
         }
-        UpdateObjectCounter();
+
+        if(_objectCounterTexts.Count != _prefabsToSpawn.Length )
+        {
+            Debug.LogError("SceneManager: Incohérence entre le nombre de Prefabs et de Compteurs");
+        }
+
+        // On peut utiliser _objectCounterTexts.Count car on a validé plus haut que c'est le même nombre que nb Prefabs
+        _nbObjects = new List<int>();
+        for(int i = 0; i < _objectCounterTexts.Count; ++i) 
+            _nbObjects.Add(0);
+
+        // Initialiser tous les compteurs (texte) à zéro
+        for(int i = 0; i < _prefabsToSpawn.Length; ++i)
+            UpdateObjectCounter(i);
     }
 
     private void SpawnOneObject()
@@ -42,22 +54,31 @@ public class SceneManager : MonoBehaviour
             Debug.LogError("SceneManager::SpawnOneObject Aucun Spawn point");
             return;
         }
-        if(_prefabToSpawn == null)
+        if(_prefabsToSpawn == null || _prefabsToSpawn.Length == 0)
         {
             Debug.LogError("Aucun prefab spécifié");
             return;
         }
 
-        _nbObjects++;
-        UpdateObjectCounter();
-        int spawnIndex = Random.Range(0, _spawnPoints.Count);
+        // Choix du point de Spawn
+        int spawnPointIndex = Random.Range(0, _spawnPoints.Count);
+        // Choix de l'objet à Spawn
+        int spawnObjectIndex = Random.Range(0, _prefabsToSpawn.Length);
 
-        GameObject.Instantiate(_prefabToSpawn, _spawnPoints[spawnIndex].transform.position, _spawnPoints[spawnIndex].transform.rotation);
+        GameObject.Instantiate(_prefabsToSpawn[spawnObjectIndex], _spawnPoints[spawnPointIndex].transform.position, _spawnPoints[spawnPointIndex].transform.rotation);
+        ++_nbObjects[spawnObjectIndex];
+
+        UpdateObjectCounter(spawnObjectIndex);
     }
 
-    private void UpdateObjectCounter()
+    private void UpdateObjectCounter(int pObjectIndex)
     {
-        _objectCounterText.text = _nbObjects.ToString();
+        if(pObjectIndex >= _objectCounterTexts.Count || pObjectIndex < 0) 
+        {
+            Debug.LogError("pObjectIndex n'est pas conforme au nombre d'objets");
+        }
+
+        _objectCounterTexts[pObjectIndex].text = _nbObjects[pObjectIndex].ToString();
     }
 
     // Update is called once per frame
